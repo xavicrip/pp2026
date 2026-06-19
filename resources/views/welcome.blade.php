c25eedea99f38d3ad721c4041686bd9e1556260c
xavicrip <wxbustamante@utpl.edu.ec>
2026-06-18 20:49:22 -0500

    agregar una rama product

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,6 +47,9 @@
                 </nav>
             @endif
         </header>
+        <div class="container">
+            <h1>Hello World</h1>
+        </div>
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                 <div class="text-[13px] leading-[20px] flex-1 p-6 pb-6 lg:p-20 lg:pb-10 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
